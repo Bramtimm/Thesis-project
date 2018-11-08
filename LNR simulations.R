0139ede4b7c18a39e6c261d599d97bc6ee58ab9a
Bramtimm <Bram.timm@gmail.com>
2018-11-08 16:06:50 +0100

    Version 0.0 (8-11-2018): no code.

---
+++ LNR simulations.R
@@ -0,0 +1,9 @@
+############## Modeling with LNR ###############################################
+
+# Author: Bram Timmers
+# use: experimenting with LNR, creating functions for simulations 1,2,3
+# Dependencies: DMC -> install_packages.R, dmc/dmc.R, load_model ("LNR","lnr.R")
+
+################################################################################
+
+
